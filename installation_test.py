1e425893d51c2b5ba4d00108ee5e5c722882d1b2
Bohan Jeff Li <qy120838@126.com>
2025-05-30 18:32:40 -0700

    Update installation_test.py to be complete.

--- installation_test.py
+++ installation_test.py
@@ -1,7 +1,16 @@
+import sys
+import numpy
+import scipy
+import jax
 import PyQt6.QtCore as qc
 import vtk
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
-print("PyQt6 version:", qc.PYQT_VERSION_STR)
-print("Qt version   :", qc.QT_VERSION_STR)
-print("VTK version  :", vtk.vtkVersion.GetVTKVersion())
+# Version checks
+print("Python version:", sys.version)
+print("NumPy version :", numpy.__version__)
+print("SciPy version :", scipy.__version__)
+print("JAX version   :", jax.__version__)
+print("PyQt6 version :", qc.PYQT_VERSION_STR)
+print("Qt version    :", qc.QT_VERSION_STR)
+print("VTK version   :", vtk.vtkVersion.GetVTKVersion())
